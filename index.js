b3bda1dbef94ebd4a25aa766af273cbdc0704709
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-03-11 20:36:57 -0500

    loops

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(name, event){
+    let message = [];
+    for(let i = 0; i < name.length; i++){
+        message[i] = `Thank you, ${name[i]}, for the wonderful ${event} gift!`;
+    }
+    return message;
+}
+
+function countDown(n){
+    while(n >= 0){
+        console.log(n);
+        n--
+    }
+    
+}
